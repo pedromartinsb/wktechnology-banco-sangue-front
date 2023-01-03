a71ee4b4c0e6e7e785cb900fc53cde382aa8927f
pedrocampos <pedro.campos@startaideia.com.br>
2023-01-03 13:35:26 -0300

    feat: criação do componente header

--- agencia-banco-sangue/src/app/app.module.ts
+++ agencia-banco-sangue/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MatCardModule } from '@angular/material/card';
 // Componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 // import { ToastrModule } from 'ngx-toastr';
 
@@ -34,7 +35,8 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
